f305591da85545cb1e94528bccbf8a6b6c6818cb
vinisnzy <manoseilacara123@gmail.com>
2025-07-21 16:52:49 -0300

    feat: implements addProductsToOrder, and add logic to update order

--- src/main/java/com/vinisnzy/order_service/service/OrderService.java
+++ src/main/java/com/vinisnzy/order_service/service/OrderService.java
@@ -1,15 +1,19 @@
 package com.vinisnzy.order_service.service;
 
 import com.vinisnzy.order_service.domain.Order;
+import com.vinisnzy.order_service.domain.OrderProduct;
 import com.vinisnzy.order_service.dto.order.OrderRequestDTO;
 import com.vinisnzy.order_service.dto.order.OrderResponseDTO;
+import com.vinisnzy.order_service.dto.product.OrderProductDTO;
 import com.vinisnzy.order_service.exceptions.ResourceNotFoundException;
 import com.vinisnzy.order_service.mapper.OrderMapper;
+import com.vinisnzy.order_service.repository.OrderProductRepository;
 import com.vinisnzy.order_service.repository.OrderRepository;
-import com.vinisnzy.order_service.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 @Service
@@ -19,35 +23,48 @@ public class OrderService {
     private OrderRepository orderRepository;
 
     @Autowired
+    private OrderProductRepository orderProductRepository;
+
+    @Autowired
+    private ProductService productService;
+
+    @Autowired
     private OrderMapper mapper;
 
     public OrderResponseDTO createOrder(OrderRequestDTO dto) {
-        Order order = mapper.orderRequestDTOToOrder(dto);
+        Order order = mapper.toEntity(dto);
+        addProductsToOrder(order, dto.items());
         order = orderRepository.save(order);
-        return mapper.orderToOrderResponseDTO(order);
+        return mapper.toDTO(order);
     }
 
     public List<OrderResponseDTO> getAllOrders() {
         List<Order> list = orderRepository.findAll();
         return list.stream()
-                .map(mapper::orderToOrderResponseDTO)
+                .map(mapper::toDTO)
                 .toList();
     }
 
     public OrderResponseDTO getOrderById(Long id) {
         Order order = findById(id);
-        return mapper.orderToOrderResponseDTO(order);
+        return mapper.toDTO(order);
     }
 
-    public void addProductToOrder(Long orderId, Long productId) {
-        // Logic to add a product to an order
-    }
-
+    @Transactional
     public OrderResponseDTO updateOrder(Long id, OrderRequestDTO dto) {
         Order order = findById(id);
-        mapper.updateOrderFromRequestDTO(dto, order);
+        mapper.update(dto, order);
+
+        order.getItems().forEach(item -> {
+            item.getProduct().getOrderProducts().remove(item);
+        });
+        order.getItems().clear();
+        orderProductRepository.deleteByOrderId(id);
+
+        addProductsToOrder(order, dto.items());
+
         order = orderRepository.save(order);
-        return mapper.orderToOrderResponseDTO(order);
+        return mapper.toDTO(order);
     }
 
     public void deleteOrder(Long id) {
@@ -58,5 +75,14 @@ public class OrderService {
     private Order findById(Long id) {
         return orderRepository.findById(id)
                 .orElseThrow(() -> new ResourceNotFoundException("Order not found with id: " + id));
+    }
+
+    private void addProductsToOrder(Order order, List<OrderProductDTO> products) {
+        products.forEach(productDTO -> {
+            var product = productService.findById(productDTO.productId());
+            OrderProduct orderProduct = new OrderProduct(order, product, productDTO.quantity(), BigDecimal.valueOf(product.getPrice()));
+            product.getOrderProducts().add(orderProduct);
+            order.addItem(orderProduct);
+        });
     }
 }
