a67239ce0c926dcae6135ad1743122a7690d35a9
vinisnzy <manoseilacara123@gmail.com>
2025-07-21 16:50:28 -0300

    feat: add items to OrderRequestDTO and OrderResponseDTO

--- src/main/java/com/vinisnzy/order_service/dto/order/OrderRequestDTO.java
+++ src/main/java/com/vinisnzy/order_service/dto/order/OrderRequestDTO.java
@@ -1,6 +1,11 @@
 package com.vinisnzy.order_service.dto.order;
 
+import com.vinisnzy.order_service.dto.product.OrderProductDTO;
+
+import java.util.List;
+
 public record OrderRequestDTO(
-        String clientName
+        String clientName,
+        List<OrderProductDTO> items
 ) {
 }
--- src/main/java/com/vinisnzy/order_service/dto/order/OrderResponseDTO.java
+++ src/main/java/com/vinisnzy/order_service/dto/order/OrderResponseDTO.java
@@ -1,15 +1,18 @@
 package com.vinisnzy.order_service.dto.order;
 
+import com.vinisnzy.order_service.dto.product.OrderProductDTO;
 import com.vinisnzy.order_service.enums.OrderStatus;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
+import java.util.List;
 
-public record OrderResponseDTO(
+public record OrderResponseDTO (
         Long id,
         String clientName,
         LocalDateTime createdAt,
         BigDecimal subtotal,
-        OrderStatus status
+        OrderStatus status,
+        List<OrderProductDTO> items
 ) {
 }
