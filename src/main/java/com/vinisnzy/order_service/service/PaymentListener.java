ff5ae40a2d2f4cae082d9190e298e701bce8ea81
vinisnzy <manoseilacara123@gmail.com>
2025-07-22 11:52:04 -0300

    feat: create consumer to listen to payment-service

---
+++ src/main/java/com/vinisnzy/order_service/service/PaymentListener.java
@@ -0,0 +1,30 @@
+package com.vinisnzy.order_service.service;
+
+import com.vinisnzy.order_service.domain.Order;
+import com.vinisnzy.order_service.enums.OrderStatus;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PaymentListener {
+
+    @Autowired
+    private OrderService orderService;
+
+    @KafkaListener(topics = "payments.success", groupId = "order-service-group")
+    public void onPaymentSuccess(String orderId) {
+        Order order = orderService.findById(Long.parseLong(orderId));
+        order.setStatus(OrderStatus.PAID);
+        orderService.save(order);
+        System.out.println("Payment successful for order ID: " + orderId);
+    }
+
+    @KafkaListener(topics = "payments.failure", groupId = "order-service-group")
+    public void onPaymentFailure(String orderId) {
+        Order order = orderService.findById(Long.parseLong(orderId));
+        order.setStatus(OrderStatus.CANCELLED);
+        orderService.save(order);
+        System.out.println("Payment failed for order ID: " + orderId);
+    }
+}
